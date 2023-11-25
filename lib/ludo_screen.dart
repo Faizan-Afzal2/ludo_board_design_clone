9c725f57aafbfa0a51065d34511bdebe431d426d
Faian Afzal <faizanafzal.gcu@gmail.com>
2023-11-25 23:48:09 +0500

    Initial commit

---
+++ lib/ludo_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:ludo_clone/my_custome_widget.dart';
+
+class MyLudo extends StatelessWidget {
+  const MyLudo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              color: Colors.grey,
+              height: 600,
+              width: 600,
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: BigContainer(color: Colors.red[700]),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: BigContainer(color: Colors.green[500]),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: BigContainer(color: Colors.yellow[700]),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: BigContainer(color: Colors.blue[700]),
+            ),
+            Positioned(
+              left: 240,
+              top: 0,
+              child: SmallContainer(
+                id: 1,
+                color: Colors.green[700],
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: 240,
+              child: SmallContainer2(
+                id: 0,
+                color: Colors.red[700],
+              ),
+            ),
+            Positioned(
+              left: 240,
+              bottom: 0,
+              child: SmallContainer(
+                id: 2,
+                color: Colors.yellow[700],
+              ),
+            ),
+            Positioned(
+              right: 0,
+              top: 240,
+              child: SmallContainer2(
+                id: 3,
+                color: Colors.blue[700],
+              ),
+            ),
+            CenterContainer()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/my_custome_widget.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+
+class BigContainer extends StatelessWidget {
+  Color? color;
+  BigContainer({super.key, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          color: color,
+          height: 240,
+          width: 240,
+        ),
+        Positioned(
+            child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              height: 150,
+              width: 150,
+            ),
+            Positioned(
+              top: 15,
+              left: 15,
+              child: CircleAvatar(
+                backgroundColor: color,
+                radius: 20,
+              ),
+            ),
+            Positioned(
+              top: 15,
+              right: 15,
+              child: CircleAvatar(
+                backgroundColor: color,
+                radius: 20,
+              ),
+            ),
+            Positioned(
+              bottom: 15,
+              left: 15,
+              child: CircleAvatar(
+                backgroundColor: color,
+                radius: 20,
+              ),
+            ),
+            Positioned(
+              bottom: 15,
+              right: 15,
+              child: CircleAvatar(
+                backgroundColor: color,
+                radius: 20,
+              ),
+            )
+          ],
+        ))
+      ],
+    );
+  }
+}
+
+class SmallContainer extends StatelessWidget {
+  Color? color;
+  int id;
+  SmallContainer({super.key, required this.color,required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      width: 120,
+      child: GridView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: 18,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 1,
+            crossAxisCount: 3,
+          ),
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: _isColor(index, id)
+                      ? color
+                      : const Color.fromARGB(255, 236, 236, 236),
+                  border: Border.all(
+                      strokeAlign: BorderSide.strokeAlignInside,
+                      color: Colors.black,
+                      width: 0.1
+                      )),
+              child: Center(
+                child: Text(index.toString()),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class SmallContainer2 extends StatelessWidget {
+  Color? color;
+  int id;
+  SmallContainer2({super.key, required this.color,required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      width: 240,
+      child: GridView.builder(
+          itemCount: 18,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 1,
+            crossAxisCount: 6,
+          ),
+          itemBuilder: (context, index) {
+            return Container(
+              child: Center(
+                child:  Text(index.toString()),
+              ),
+              decoration: BoxDecoration(
+                  color:_isColor(index, id)? color:Colors.white,
+                  border: Border.all(
+                      strokeAlign: BorderSide.strokeAlignInside,
+                      color: Colors.black,
+                      width: 0.1
+                      ),),
+                      
+            );
+          }),
+    );
+  }
+}
+
+class CenterContainer extends StatelessWidget {
+  const CenterContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+              height: 120,
+              width: 120,
+        ),
+        // Green
+      Positioned(
+        left: 25,
+        top: -39,
+        child: CircleAvatar(
+          backgroundColor: Colors.green[700],
+          radius: 35,
+        ),
+      ), 
+
+      // Red
+      Positioned(
+        left: -39,
+        top: 25,
+        child: CircleAvatar(
+          backgroundColor: Colors.red[700],
+          radius: 35,
+        ),
+      ),
+      // Blue
+      Positioned(
+        right: -39,
+        top: 25,
+        child: CircleAvatar(
+          backgroundColor: Colors.blue[700],
+          radius: 35,
+        ),
+      ),
+      // Yellow
+      Positioned(
+        bottom: -39,
+        left: 25,
+        child: CircleAvatar(
+          backgroundColor: Colors.yellow[700],
+          radius: 35,
+        ),
+      ),
+      ],
+    );
+  }
+}
+
+
+bool _isColor(int index, int id) {
+  switch (id) {
+    case 0:
+    // In case of red color
+      switch (index) {
+        case 1:
+          return true;
+        case 7:
+          return true;
+        case 8:
+          return true;
+        case 9:
+          return true;
+        case 10:
+          return true;
+        case 11:
+          return true;
+        default:
+          return false;
+      }
+    case 1:
+    // In case of green color
+      switch (index) {
+        case 4:
+          return true;
+        case 5:
+          return true;
+        case 7:
+          return true;
+        case 10:
+          return true;
+        case 13:
+          return true;
+        case 16:
+          return true;
+        default:
+          return false;
+      }
+    case 2:
+    // In case of yellow color
+      switch (index) {
+        case 4:
+          return true;
+        case 12:
+          return true;
+        case 7:
+          return true;
+        case 10:
+          return true;
+        case 13:
+          return true;
+        case 1:
+          return true;
+
+        default:
+          return false;
+      }
+    case 3:
+    // In case of blue color
+      switch (index) {
+        case 6:
+          return true;
+        case 8: 
+          return true;
+        case 7:
+          return true;
+        case 9:
+          return true;
+        case 10:
+          return true;
+        case 16:
+          return true;
+        default:
+          return false;
+      }
+
+    default:
+      return true;
+  }
+}
